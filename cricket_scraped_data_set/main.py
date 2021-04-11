800e363974a6c5243918697dbe000a643eb65b0b
sahil <souravjangar91@gmail.com>
2021-04-11 19:48:24 +0530

    fixes different match fixes

--- cricket_scraped_data_set/main.py
+++ cricket_scraped_data_set/main.py
@@ -1,15 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 import time 
+import re
 
 def all_time_run_delaye():
-    pass # may be updater that update or re check the values ---------------------------
+    pass
 
-def delayer():
-    pass #to delaye the function so that server won't block the program--------------------------------
 
-def type_of_matches_played():
-    pass #------------------------------to find how many of them will play odi, test , t20i-----------------------------
+def type_of_matches_played(soup_player_specific):
+    all_matches_played = soup_player_specific.find_all("h4", class_= "player-stats__table-match-text")
+    matches = []
+    for tag in all_matches_played:
+        item = tag.text
+        matches.append(item.lower())
+    if len(matches) == 6:
+        for i in range(3):
+            matches.pop()
+    elif len(matches) == 4:
+        for i in range(2):
+            matches.pop()
+    return matches
+
 
 def data_getters(players_table_test):
     player_bb_test_list1 = []
@@ -46,6 +57,10 @@ def players_id_getter(players_all_tag):
     return players_id
 
 
+def delayer():
+    time.sleep(0.5)
+
+
 if __name__ == "__main__":
     url_team_specific = "https://www.bcci.tv/players/men"
     res_team_specific = requests.get(url_team_specific)
@@ -55,23 +70,22 @@ if __name__ == "__main__":
 
     players_all_tag = soup_team_specific.find_all("a",class_="player-item")
     players_id = players_id_getter(players_all_tag)
-    #print(players_id)-------------------ok till here -----------------------------------
+    
 
     for tag in players_id:
         url_player_specific = f"https://www.bcci.tv{tag}"
         res_player_specific = requests.get(url_player_specific)
         res_html = res_player_specific.text
         soup_player_specific = BeautifulSoup(res_html, "html.parser")
-        matches_list = ["test","odi","t20i"]
+        matches = type_of_matches_played(soup_player_specific)
         print("\n\n")
-        for match in matches_list:
+        for match in matches:
             path = f"player-stats__table-row t-{match}"
             players_table = soup_player_specific.find_all("tr", class_=path)
             data = data_getters(players_table)
+            delayer()
             data_bat, data_bowl = splitter(data)
             print(f"batting for {match}")
             print(data_bat)
             print(f"bowling for {match}")
             print(data_bowl)
-    
-    
